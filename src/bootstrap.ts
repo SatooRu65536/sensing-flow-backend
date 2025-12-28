f5d4fa0b93b218def60b9e09a0310b22709bd1f7
SatooRu65536 <ryuto65536@gmail.com>
2025-12-28 22:16:01 +0900

    feat: swagger

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export async function bootstrap() {
   const nestApp = await NestFactory.create(AppModule);
@@ -22,9 +23,20 @@ export async function bootstrap() {
   nestApp.enableShutdownHooks();
   nestApp.use(bodyParser.json({ limit: '15mb' }));
 
+  if (process.env.ENV_DEV === 'true') {
+    const config = new DocumentBuilder()
+      .setTitle('Sensing Flow API')
+      .setDescription('API documentation for Sensing Flow')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(nestApp, config);
+    SwaggerModule.setup('docs', nestApp, document);
+  }
+
   await nestApp.init();
 
-  if (!process.env.VITE_START) {
+  if (process.env.ENV_DEV === 'true') {
     await nestApp.listen(3000);
   }
 
